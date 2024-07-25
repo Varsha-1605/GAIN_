0f367d990ac280471c9e4b43846369097672fae1
Saahil Ahmad <130739587+iSaahilx@users.noreply.github.com>
2024-07-25 22:49:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,13 @@ const adminRoutes = require("./src/routes/adminRoutes");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://gain-pi.vercel.app/"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
