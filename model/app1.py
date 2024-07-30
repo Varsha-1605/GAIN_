775b67ce2af7b94ddb9c73b13618765694c5f0ea
Saahil Ahmad <130739587+iSaahilx@users.noreply.github.com>
2024-07-30 22:54:59 +0530

    Update app1.py

--- model/app1.py
+++ model/app1.py
@@ -153,6 +153,5 @@ def home():
     return "Hello Welcome to our Finance app GAIN"
     
     
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host=os.getenv('HOST'), port=int(os.getenv('PORT'),8081), debug=False)
