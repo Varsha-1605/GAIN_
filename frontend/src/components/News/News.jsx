9d893e9164d158319c619e60ccc3b4f87ada3713
Saahil Ahmad <130739587+iSaahilx@users.noreply.github.com>
2024-08-22 03:19:02 +0530

    Update News.jsx

--- frontend/src/components/News/News.jsx
+++ frontend/src/components/News/News.jsx
@@ -1,38 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Card, CardActionArea, CardContent, CardMedia, Typography, Grid, Container, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
-import StockChart from '../StockChart/StockChart.jsx';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const theme = createTheme({
   typography: {
-  
     h5: {
       fontFamily: 'Arial, sans-serif',
       fontWeight: 500,
       fontSize: '1rem',
     },
   },
+  palette: {
+    background: {
+      default: 'black',
+    },  
+
+  },
 });
 
-const stockSymbols = [
-  { symbol: 'AAPL', name: 'Apple Inc.' },
-  { symbol: 'AMZN', name: 'Amazon.com Inc.' },
-  { symbol: 'GOOGL', name: 'Google Inc.' },
-  { symbol: 'MSFT', name: 'Microsoft Corporation' },
-  { symbol: 'TSLA', name: 'Tesla, Inc.' },
-  { symbol: 'RELI', name: 'Reliance Industries Limited' },
-  { symbol: 'TCS', name: 'Tata Consultancy Services' },
-  { symbol: 'INFY', name: 'Infosys Limited' },
-  { symbol: 'HDB', name: 'HDFC Bank Limited' },
-  { symbol: 'WIT', name: 'Wipro Limited' },
-];
+
+
 
 
 function News() {
   const [news, setNews] = useState([]);
-  const [selectedStock, setSelectedStock] = useState(stockSymbols[0].symbol);
+
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -55,10 +49,10 @@ function News() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container>
-      <div className="header" style={{marginBottom:20}}>
-        <h1>Latest News</h1>
-      </div>
+      <Container  style={{color: 'white', minHeight: '100vh', marginTop: 0, paddingTop:0 }}>
+      <Typography variant="h4" align="center" gutterBottom sx={{ mt: 5,mb:3 }}>
+            Latest News
+          </Typography>
         <Grid container spacing={4} justifyContent="center">
           {news.map((article, index) => (
             <Grid item key={index} sx={{ mb: 2 }}>
@@ -86,26 +80,7 @@ function News() {
           ))}
         </Grid>
 
-        <div className="header" style={{marginBottom:20}}>
-        <h1>Stock Charts</h1>
-      </div>
-
-        <FormControl sx={{ mb: 2, minWidth: 200 }}>
-          <InputLabel>Company</InputLabel>
-          <Select
-            value={selectedStock}
-            onChange={(e) => setSelectedStock(e.target.value)}
-            label="Company"
-          >
-            {stockSymbols.map((stock) => (
-              <MenuItem key={stock.symbol} value={stock.symbol}>
-                {stock.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-
-        <StockChart symbol={selectedStock} />
+        
       </Container>
     </ThemeProvider>
   );
